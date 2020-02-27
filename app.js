8872fe487382255326310597ee6d35cf2c1e3c11
Marcel <fleuren.marcel@gmail.com>
2020-02-27 13:27:06 +0100

    added styled, styleguide and hbs

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const mongoose = require('mongoose');
+const express = require('express')
+const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const mongoose = require('mongoose')
+const compression = require('compression')
+const path = require('path')
+const hbs = require('express-handlebars');
+const app = express();
 
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolvers = require('./graphql/resolvers/index');
-const isAuth = require('./middleware/is-auth');
+const graphQlSchema = require('./graphql/schema/index')
+const graphQlResolvers = require('./graphql/resolvers/index')
+const isAuth = require('./middleware/is-auth')
 
-const app = express()
+// static files serving
 app
+  .use(express.static(path.join(__dirname, '/public')))
+  .use(compression())
   .use(bodyParser.json())
   .use(isAuth)
   .use(
@@ -20,8 +26,28 @@ app
   })
 );
 
+app
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'hbs')
+  .engine('hbs', hbs({
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname +
+    '/views/layouts/',
+}))
+
+
+// import routes
+const index = require('./routes/index');
+
+app
+  .get('/', index)
+
 mongoose
-  .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-xn2pr.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+  .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-xn2pr.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     app.listen(3001)
   }).catch(err => {
